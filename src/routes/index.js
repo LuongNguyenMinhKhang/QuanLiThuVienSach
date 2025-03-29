eee731fa14228221a738cfc2b795b26fe28f565d
LuongNguyenMinhKhang <luongnmkhang@gmail.com>
2025-03-30 03:01:50 +0700

    Register Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const shopingRouter = require('./shopingcart.js');
 const loginRouter = require('./login.js');
 const homeRouter = require('./home.js');
 const userRouter = require('./user.js');
+const registerRouter = require('./register.js');
 
 function route(app) {
     app.use('/shopingcart', shopingRouter);
     app.use('/login', loginRouter);
     app.use('/user', userRouter);
+    app.use('/register', registerRouter);
     app.use('/contact', contactRouter);
     app.use('/home', homeRouter);
     app.use('/', homeRouter);
